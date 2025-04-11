165d749f44d66ddbe7c32f3869d0d5a0d3e344ea
Hargun Singh <hargunsingh@Harguns-MacBook-Air.local>
2025-04-12 11:09:01 +1200

    Added two new methods and a list of type Review to manage reviews

--- src/main/java/nz/ac/auckland/se281/Activity.java
+++ src/main/java/nz/ac/auckland/se281/Activity.java
@@ -1,9 +1,13 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Activity {
   private String name; // The name of the activity
   private Types.ActivityType type; // The type of the activity
   private String activityId; // The unique ID of the activity
+  private List<Review> reviews = new ArrayList<>();
 
   public Activity(String name, Types.ActivityType type, String activityId) {
     this.name = name;
@@ -21,5 +25,13 @@ public class Activity {
 
   public String getActivityId() {
     return activityId;
+  }
+
+  public List<Review> getReviews() {
+    return reviews;
+  }
+
+  public void addReview(Review review) {
+    reviews.add(review);
   }
 }
