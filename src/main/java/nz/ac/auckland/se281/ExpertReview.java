84d38b8a5d765c0d31d5bfc1d449c10b7f01590d
Hargun Singh <hargunsingh@Harguns-MacBook-Air.local>
2025-04-13 09:50:43 +1200

    Updated ExpertReview to include an ArraylistList to store images, a new method to add images and an if statement to check and display the message appropriately

--- src/main/java/nz/ac/auckland/se281/ExpertReview.java
+++ src/main/java/nz/ac/auckland/se281/ExpertReview.java
@@ -1,12 +1,21 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExpertReview extends Review {
   private boolean recommended;
+  private List<String> images;
 
   public ExpertReview(
       String reviewId, String reviewerName, int rating, String comments, boolean recommended) {
     super(reviewId, reviewerName, rating, comments);
     this.recommended = recommended;
+    this.images = new ArrayList<>();
+  }
+
+  public void addImage(String imageName) {
+    images.add(imageName);
   }
 
   @Override
@@ -17,6 +26,9 @@ public class ExpertReview extends Review {
     MessageCli.REVIEW_ENTRY_REVIEW_TEXT.printMessage(comments);
     if (recommended) {
       MessageCli.REVIEW_ENTRY_RECOMMENDED.printMessage();
+    }
+    if (images != null && !images.isEmpty()) {
+      MessageCli.REVIEW_ENTRY_IMAGES.printMessage(String.join(",", images)); // Use String.join
     }
   }
 }
